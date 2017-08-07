9f1904202b62cd3fa71805031658ee83645e5052
levsthings <bearnode@gmail.com>
2017-08-08 02:22:10 +0300

    replace request with superagent due to better performance

--- src/functions/contactForm/post.js
+++ src/functions/contactForm/post.js
@@ -1,22 +1,19 @@
 'use strict'
 const {config} = require('../../config/')
-const rp = require('request-promise')
+const request = require('superagent')
 
 exports.post = (data) => {
-    let options = {
-        method: 'POST',
+    let relay = {
         uri: config.hookURL,
         body: {
             channel: '#_test-lab_',
             username: 'Cactus',
             text: `${data.info} has sent us a message\n Name: ${data.body.name}\n Email: ${data.body.email}\n Subject: ${data.body.subject}\n Message: ${data.body.message}`,
             icon_emoji: ':cactus:'
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        resolveWithFullResponse: true
+        }
     }
-    return rp(options)
+    return request
+        .post(relay.uri)
+            .set('Content-Type', 'application-json')
+            .send(JSON.stringify(relay.body))
 }
