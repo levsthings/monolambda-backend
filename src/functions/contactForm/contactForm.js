0930664669a76c06fe61585adf748f4d81e2730c
levsthings <bearnode@gmail.com>
2017-08-22 13:11:57 +0300

    catch errors, switch to production mode

--- src/functions/contactForm/contactForm.js
+++ src/functions/contactForm/contactForm.js
@@ -3,17 +3,20 @@ const {postRequest} = require('./post')
 const {response} = require('./response')
 
 exports.handleMessage = (event, context, callback) => {
-    const data = {
-        body: JSON.parse(event.body),
-        info: event.requestContext.identity.sourceIp
+    try {
+        let data = {
+            body: JSON.parse(event.body),
+            info: event.requestContext.identity.sourceIp
+        }
+        postRequest(data)
+            .then((res) => {
+                (res === 'ok')
+                    ? callback(null, response(200, 'OK'))
+                    : callback(null, response(400, 'FAIL'))
+            })
+            .catch(res => callback(null, response(400, 'FAIL')))
+    } catch (error) {
+        console.error(error)
+        callback(null, response(400, 'INVALID DATA'))
     }
-    postRequest(data)
-        .then((res) => {
-            (res === 'ok')
-                ? callback(null, response(200, 'OK'))
-                : callback(null, response(400, 'FAIL'))
-        })
-        .catch((res) => {
-            callback(null, response(400, 'FAIL'))
-        })
 }
