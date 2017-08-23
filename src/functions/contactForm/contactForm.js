d46a3512e377c976b7a6c9c1b4f169e8507ab8e5
levsthings <bearnode@gmail.com>
2017-08-23 22:16:07 +0300

    add cron job for thawing lambda function

--- src/functions/contactForm/contactForm.js
+++ src/functions/contactForm/contactForm.js
@@ -3,23 +3,28 @@ const {postRequest} = require('./post')
 const {response} = require('./response')
 
 exports.handleMessage = (event, context, callback) => {
-    try {
-        let data = {
-            body: JSON.parse(event.body),
-            info: event.requestContext.identity.sourceIp
+    if (event.source === 'serverless-plugin-warmup') {
+        console.log('Thawing lambda...')
+        callback(null, 'Lambda is warm!')
+    } else {
+        try {
+            let data = {
+                body: JSON.parse(event.body),
+                info: event.requestContext.identity.sourceIp
+            }
+            postRequest(data)
+                .then((res) => {
+                    (res === 200)
+                        ? callback(null, response(200, 'OK'))
+                        : callback(null, response(400, 'FAIL'))
+                })
+                .catch((error) => {
+                    console.error(error)
+                    callback(null, response(400, 'FAIL'))
+                })
+        } catch (error) {
+            console.error('INVALID DATA', error)
+            callback(null, response(400, 'INVALID DATA'))
         }
-        postRequest(data)
-            .then((res) => {
-                (res === 200)
-                    ? callback(null, response(200, 'OK'))
-                    : callback(null, response(400, 'FAIL'))
-            })
-            .catch((error) => {
-                console.error(error)
-                callback(null, response(400, 'FAIL'))
-            })
-    } catch (error) {
-        console.error('INVALID DATA', error)
-        callback(null, response(400, 'INVALID DATA'))
     }
 }
